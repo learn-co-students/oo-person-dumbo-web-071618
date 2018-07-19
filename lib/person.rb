b6ddf5283be3be18ccacfd927d48ca3afdc7c9b2
joalorro <joalorro@gmail.com>
2018-07-18 20:00:41 -0400

    finished

--- lib/person.rb
+++ lib/person.rb
@@ -38,24 +38,13 @@ class Person
     self.happiness > 7 ? true : false
   end
   
-  # def mod_pts(action, num,stats)
-  #   if action == "Increase"
-  #     stats + num <= 10 ? stats += num : stats = 10
-  #   else
-  #     stats - num >= 0 ? stats -= num : stats = 0
-  #   end
-  # end
-  
   def take_bath
     self.hygiene += 4
     return "♪ Rub-a-dub just relaxing in the tub ♫"
   end
   
   def work_out
-    # @happiness = mod_pts("Increase", 2, @happiness)
     self.happiness += 2
-
-    # @hygiene = mod_pts("Decrease", 3, @hygiene)
     self.hygiene -= 3
     "♪ another one bites the dust ♫"
   end
@@ -65,19 +54,31 @@ class Person
   	"all about the benjamins"
   end
 
-  def call_friend
-
+  def call_friend(friend)
+  	self.happiness += 3
+  	friend.happiness += 3
+  	return "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
 
-  def start_conversation(topic)
+  def start_conversation(person,topic)
   	
+  	case topic 
+
+  		when "politics"
+
+  			self.happiness -= 2
+  			person.happiness -= 2
+  			return 'blah blah partisan blah lobbyist'
+
+  		when "weather"
+  			self.happiness += 1 
+  			person.happiness +=1
+  			return 'blah blah sun blah rain'
+  		else 
+  			return 'blah blah blah blah blah'
+  	end
 
   end
 
 
 end
-
-john = Person.new("John") 
-john.take_bath
-
-puts john.hygiene
