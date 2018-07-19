6654cc760ed25c4d42aab8b2071a3111790e6a49
Jason Pitts <juniorjr083@gmail.com>
2018-07-19 12:14:06 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # your code goes here
 class Person 
   
@@ -101,3 +102,80 @@ def start_conversation(person, topic)
 end 
 
 end
+=======
+
+# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness_points, :hygeine_points
+
+    def intialize(name)
+    @name = name 
+    @bank_account = 25
+    @happiness = 8 
+    @hygeine = 8 
+    
+end
+
+
+def happiness=(value)
+    if value  > 10 
+        @happiness = 10 
+
+    elsif value < 0 
+        
+        @happiness = 0
+
+    end 
+
+end 
+
+def hygeine=(value= 4)
+    if value  > 10 
+        @hygeine = 10 
+
+    elsif value < 0 
+        
+        @hygeine = 0
+
+    end 
+
+end  
+
+
+
+ def clean?
+ @hygeine > 7 
+ 
+ end 
+
+ def happy?
+ 
+    @happiness > 7 
+
+ end 
+
+
+ def get_paid(salary)
+    @bank_account += salary
+    "All about the benjamins"
+
+ end
+
+ def take_bath
+   
+ (@hygeine += 4)  > 10  ? @hygeine = 10 : @hygeine += 4
+ "♪ Rub-a-dub just relaxing in the tub ♫"
+end 
+
+def work_out
+    #(@happiness += 2) > 10 ? @happiness = 10 : @happiness += 2 
+    #(@hygeine -= 3) <= 0 ? @hygeine =0 : @hygeine -= 3
+    
+    
+    
+    "♪ another one bites the dust ♫"
+end 
+
+end
+>>>>>>> acd1a4246787d686fa88f3a24e36f60f64db341e
